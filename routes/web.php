c2dfdc597252b654c3103a2b527e00b5ce396345
Mark Miller <constenum@gmail.com>
2016-11-25 15:52:11 -0500

    Add tutor routes

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,30 @@ Route::get('/students/{id}/delete', 'StudentController@delete')->name('students.
 
 # Delete route to actually destroy the students
 Route::delete('/students/{id}', 'StudentController@destroy')->name('students.destroy');
+
+/**
+* Tutor resource
+*/
+# Index page to show all tutors
+Route::get('/tutors', 'TutorController@index')->name('tutors.index');
+
+# Show form to create a new tutors
+Route::get('/tutors/create', 'TutorController@create')->name('tutors.create');
+
+# Process the form to create a new tutors
+Route::post('/tutors', 'TutorController@store')->name('tutors.store');
+
+# Show an individual tutors
+Route::get('/tutors/{title}', 'TutorController@show')->name('tutors.show');
+
+# Show form to edit a tutors
+Route::get('/tutors/{id}/edit', 'TutorController@edit')->name('tutors.edit');
+
+# Process form to edit a tutors
+Route::put('/tutors/{id}', 'TutorController@update')->name('tutors.update');
+
+# Get route to confirm deletion of tutors
+Route::get('/tutors/{id}/delete', 'TutorController@delete')->name('tutors.destroy');
+
+# Delete route to actually destroy the tutors
+Route::delete('/tutors/{id}', 'TutorController@destroy')->name('tutors.destroy');
