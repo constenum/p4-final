2adc1d32d472af304370bc33de10187cf999b8fa
Mark Miller <constenum@gmail.com>
2016-12-08 01:22:41 -0500

    Update ModelFactory and DatabaseSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,26 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Student::class, function (Faker\Generator $faker) {
+    return [
+        'student_number' => $faker->numberBetween($min = 2017001, $max = 2023001),
+        'first_name' => $faker->firstName($gender = 'female'),
+        'last_name' => $faker->lastName,
+        'card_number' => $faker->numberBetween($min = 1617001, $max = 1617999),
+    ];
+});
+
+$factory->define(App\Subject::class, function (Faker\Generator $faker) {
+    return [
+        'subject' => $faker->word,
+    ];
+});
+
+$factory->define(App\Session::class, function (Faker\Generator $faker) {
+    return [
+        'subject_id' => $faker->randomElement(App\Subject::pluck('id')->toArray()),
+        'student_id' => $faker->randomElement(App\Student::pluck('id')->toArray()),
+        'user_id' => $faker->randomElement(App\User::pluck('id')->toArray()),
+    ];
+});
