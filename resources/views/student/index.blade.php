f63d56284df8bf916ca40cdf7294d3be3b5a64cb
Mark Miller <constenum@gmail.com>
2016-12-08 22:05:42 -0500

    Clean up spacing

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -11,29 +11,29 @@
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
-                      <th>Student Number</th>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Card Number</th>
-                      <th></th>
-                      <th></th>
+                        <th>Student Number</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Card Number</th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($students as $student)
                         <tr>
-                          <td>{{ $student->student_number }}</td>
-                          <td>{{ $student->first_name }}</td>
-                          <td>{{ $student->last_name}}</td>
-                          <td>{{ $student->card_number }}</td>
-                          <td>
-                              <a class="button btn btn-default" href="/students/{{ $student->id }}/edit">
-                                  <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
-                          </td>
-                          <td>
-                              <a class="button btn btn-default" href="/students/{{ $student->id }}/delete">
-                                  <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
-                          </td>
+                            <td>{{ $student->student_number }}</td>
+                            <td>{{ $student->first_name }}</td>
+                            <td>{{ $student->last_name}}</td>
+                            <td>{{ $student->card_number }}</td>
+                            <td>
+                                <a class="button btn btn-default" href="/students/{{ $student->id }}/edit">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
+                            </td>
+                            <td>
+                                <a class="button btn btn-default" href="/students/{{ $student->id }}/delete">
+                                    <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
