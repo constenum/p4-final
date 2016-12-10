327ddabeaa6d529a969000d1583665ab2182910e
Mark Miller <constenum@gmail.com>
2016-12-09 22:25:36 -0500

    Add session view and Update controller index method

--- app/Session.php
+++ app/Session.php
@@ -12,21 +12,21 @@ class Session extends Model
     * Each seseion has one subject
     */
     public function subject() {
-        return $this->belongsToMany('App\Subject');
+        return $this->belongsTo('App\Subject');
     }
 
     /**
     * Each seseion has one student
     */
     public function student() {
-        return $this->belongsToMany('App\Student');
+        return $this->belongsTo('App\Student');
     }
 
     /**
     * Each seseion has one user
     */
     public function user() {
-        return $this->belongsToMany('App\User');
+        return $this->belongsTo('App\User');
     }
 
     /* End Relationship Method */
---
+++ resources/views/session/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container">
+        @if(sizeof($sessions) == 0)
+            <h3>There are no sessions in the system. Please contact tech support if you believe this is incorrect.</h3>
+        @else
+            <h1 class="text-center table-name">Sessions</h1>
+
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Session ID</th>
+                        <th>Student Name</th>
+                        <th>Subject</th>
+                        <th>Session Start</th>
+                        <th>Session end</th>
+                        <th>Tutor Name</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($sessions as $session)
+                        <tr>
+                            <td>{{ $session->id }}</td>
+                            <td>{{ $session->student->first_name }} {{ $session->student->last_name }}</td>
+                            <td>{{ $session->subject->subject }}</td>
+                            <td>{{ $session->created_at }}</td>
+                            <td>{{ $session->updated_at }}</td>
+                            <td>{{ $session->user->name }}</td>
+                            <td>
+                                <a class="button btn btn-default" href="/tutor/{{ $session->id }}/edit">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
+                            </td>
+                            <td>
+                                <a class="button btn btn-default" href="/tutor/{{ $session->id }}/delete">
+                                    <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
+@endsection
