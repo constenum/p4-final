e546427b72aafe50e8e3a0f8711766e40db0ce33
Mark Miller <constenum@gmail.com>
2016-12-09 23:47:01 -0500

    Correct path folder names

--- resources/views/subject/index.blade.php
+++ resources/views/subject/index.blade.php
@@ -8,7 +8,7 @@
         @else
             <h1 class="text-center table-name">Subjects</h1>
 
-            <a class="button btn btn-default" href="/subjects/create">
+            <a class="button btn btn-default" href="/subject/create">
                 <i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Subject</a>
 
             <table class="table table-striped table-hover">
@@ -26,11 +26,11 @@
                             <td>{{ $subject->id }}</td>
                             <td>{{ $subject->subject }}</td>
                             <td>
-                                <a class="button btn btn-default" href="/subjects/{{ $subject->id }}/edit">
+                                <a class="button btn btn-default" href="/subject/{{ $subject->id }}/edit">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
                             </td>
                             <td>
-                                <a class="button btn btn-default" href="/subjects/{{ $subject->id }}/delete">
+                                <a class="button btn btn-default" href="/subject/{{ $subject->id }}/delete">
                                     <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
                             </td>
                         </tr>
--- resources/views/tutor/index.blade.php
+++ resources/views/tutor/index.blade.php
@@ -8,7 +8,7 @@
         @else
             <h1 class="text-center table-name">Tutors</h1>
 
-            <a class="button btn btn-default" href="/users/create">
+            <a class="button btn btn-default" href="/tutor/create">
                 <i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Tutor</a>
 
             <table class="table table-striped table-hover">
@@ -28,11 +28,11 @@
                             <td>{{ $tutor->name }}</td>
                             <td>{{ $tutor->email }}</td>
                             <td>
-                                <a class="button btn btn-default" href="/users/{{ $tutor->id }}/edit">
+                                <a class="button btn btn-default" href="/tutor/{{ $tutor->id }}/edit">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>
                             </td>
                             <td>
-                                <a class="button btn btn-default" href="/users/{{ $tutor->id }}/delete">
+                                <a class="button btn btn-default" href="/tutor/{{ $tutor->id }}/delete">
                                     <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a>
                             </td>
                         </tr>
