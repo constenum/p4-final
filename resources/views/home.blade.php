67f4020ae73dea3c56541400f0a1e7ca1d9d2f30
Mark Miller <constenum@gmail.com>
2016-12-18 15:23:03 -0500

    Move new tutoring session button so it alway show

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="container">
+    <h1 class="text-center table-name">Open Tutoring Sessions</h1>
+
+    <a class="button btn btn-default" href="/sessions/create">
+        <i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Tutoring Session</a>
+
     @if(sizeof($sessions) == 0)
         <h3>You have no open tutoring sessions. Please contact tech support if you believe this is incorrect.</h3>
     @else
-        <h1 class="text-center table-name">Open Tutoring Sessions</h1>
-
-        <a class="button btn btn-default" href="/sessions/create">
-            <i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Tutoring Session</a>
-
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
